09e18aec962029722da1d0e4f25c9e3b4ec70fb1
Pawel <p.zarzycki@versamind.com>
2026-08-04 12:29:41 -0700

    chore: stop tracking generated artifacts

--- docs/website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/data1/b-parking-api/docs/website/node_modules/infima/dist/css/default/default.css"),
-  require("/data1/b-parking-api/docs/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/data1/b-parking-api/docs/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/data1/b-parking-api/docs/website/src/css/custom.css"),
-];
